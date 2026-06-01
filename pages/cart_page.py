2125e59daf8fadb01a29353539d224833297b4e3
luluhsnl <luthfiyahlulu2006@gmail.com>
2026-06-01 14:35:43 +0700

    fix: click for cart and checkout buttons

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,20 +1,20 @@
+# pages/cart_page.py
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
 class CartPage(BasePage):
-    #  Locators 
     CART_ITEMS    = (By.CLASS_NAME, 'cart_item')
     CHECKOUT_BTN  = (By.ID, 'checkout')
     CONTINUE_BTN  = (By.ID, 'continue-shopping')
 
-    #  Actions 
     def click_checkout(self):
-        self.click(self.CHECKOUT_BTN)
+        btn = self.find_clickable(self.CHECKOUT_BTN)
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+        self.driver.execute_script("arguments[0].click();", btn)
 
     def continue_shopping(self):
         self.click(self.CONTINUE_BTN)
 
-    #  Assertion Helpers 
     def get_item_count(self):
         return len(self.driver.find_elements(*self.CART_ITEMS))
 
--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,4 +1,4 @@
-
+# pages/inventory_page.py
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from pages.base_page import BasePage
@@ -6,7 +6,6 @@ from pages.base_page import BasePage
 class InventoryPage(BasePage):
     URL = 'https://www.saucedemo.com/inventory.html'
 
-    #  Locators 
     SORT_DROPDOWN    = (By.CLASS_NAME, 'product_sort_container')
     PRODUCT_NAMES    = (By.CLASS_NAME, 'inventory_item_name')
     PRODUCT_PRICES   = (By.CLASS_NAME, 'inventory_item_price')
@@ -17,27 +16,27 @@ class InventoryPage(BasePage):
     BURGER_MENU      = (By.ID, 'react-burger-menu-btn')
     LOGOUT_LINK      = (By.ID, 'logout_sidebar_link')
 
-    #  Actions 
     def sort_by(self, option):
-        """option: 'az', 'za', 'lohi', 'hilo'"""
         Select(self.find(self.SORT_DROPDOWN)).select_by_value(option)
 
     def add_product_to_cart(self, index=0):
         btns = self.driver.find_elements(*self.ADD_TO_CART_BTNS)
-        btns[index].click()
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btns[index])
+        self.driver.execute_script("arguments[0].click();", btns[index])
 
     def remove_product_from_cart(self, index=0):
         btns = self.driver.find_elements(*self.REMOVE_BTNS)
-        btns[index].click()
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btns[index])
+        self.driver.execute_script("arguments[0].click();", btns[index])
 
     def go_to_cart(self):
-        self.click(self.CART_ICON)
+        btn = self.find(self.CART_ICON)
+        self.driver.execute_script("arguments[0].click();", btn)
 
     def logout(self):
         self.click(self.BURGER_MENU)
         self.click(self.LOGOUT_LINK)
 
-    #  Assertion Helpers 
     def get_product_count(self):
         return len(self.driver.find_elements(*self.PRODUCT_NAMES))
 
